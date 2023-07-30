62d5492160b54040eb790377e6a9f170268f6033
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-07-30 12:34:36 -0400

    Rendered the Cube

--- three.js
+++ three.js
@@ -1,11 +1,5 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -22,6 +16,11 @@ camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
 	renderer.render( scene, camera );
 }
+
 animate();
