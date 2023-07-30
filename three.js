edb292f8022a0c57dd1caca161519f14b3d88d58
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-07-30 12:44:13 -0400

    adding the javascript for new scene

--- three.js
+++ three.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,10 +8,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
@@ -24,3 +21,15 @@ function animate() {
 }
 
 animate();
+
+const loader = new GLTFLoader();
+
+loader.load( 'scene.gltf', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
