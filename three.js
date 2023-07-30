37063371823bac40f6597bfaa4424b0f5d5c0454
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2023-07-30 12:07:57 -0400

    Scene and Camera

--- three.js
+++ three.js
@@ -6,3 +6,9 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
